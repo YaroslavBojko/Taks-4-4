463004927e31577c8d81a4ae409d668bd6dbb99c
Ярослав <144372173+YaroslavBojko@users.noreply.github.com>
2025-01-22 18:35:41 +0300

    count the level

--- main.cpp
+++ main.cpp
@@ -6,4 +6,24 @@ int main()
 {
 	setlocale(LC_ALL, "Rus");
 
+    int experiencePoints;
+    std::cout << "Введите количество очков опыта: ";
+    std::cin >> experiencePoints;
+    std::cout << "-----Мы считаем-----" << std::endl;
+
+    int level = 1;
+    if (experiencePoints >= 5000)
+    {
+        level += 3;
+    }
+    else if (experiencePoints >= 2500)
+    {
+        level += 2;
+    }
+    else if (experiencePoints >= 1000)
+    {
+        level += 1;
+    }
+
+    cout << "Ваш уровень: " << level << endl;
 }
